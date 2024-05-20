78268ae29f62653d165066d838c09a405d544bb0
soundiata12 <157310260+soundiata12@users.noreply.github.com>
2024-05-20 00:18:34 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "keita-persos3"
-    key            = "test/terraform.tfstate"
+    key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "state-log"
